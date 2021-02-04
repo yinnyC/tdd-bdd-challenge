eacbced3e36e0dcbefb2685039f9c54ecf6ce8ee
Yin <yinnnn17@gmail.com>
2021-02-03 21:03:17 -0800

    Finished Challenge 2 & stretch

--- tests/utils.test.js
+++ tests/utils.test.js
@@ -44,9 +44,6 @@ it("should return the area of the circle", function() {
   expect(circleArea).to.equal(Math.PI * 16)
 })
 
-
-
-
 // ========================================================
 // Level 2 Challenges
 // ========================================================
@@ -68,20 +65,77 @@ it("Should create a new (object) Item with name and price", function() {
   expect(item).to.have.property("quantity", 1)
 })
 
-it("Should return an array containing all items in cart")
+it("Should return the number of items in the cart",function(){
+  const item = utils.createItem("apple", 0.99)
+  utils.addItemToCart(item)
+  let numOfItems = utils.getNumItemsInCart()
+  expect(numOfItems).to.be.a("number")
+  expect(numOfItems).to.equal(1)
+  const item2 = utils.createItem("Banana", 0.99)
+  utils.addItemToCart(item)
+  utils.addItemToCart(item2)
+  numOfItems = utils.getNumItemsInCart()
+  expect(numOfItems).to.equal(3)
+})
 
-it("Should add a new item to the shopping cart")
+it("Should return an array containing all items in cart", function(){
+  const item = utils.createItem("apple", 0.99)
+  utils.addItemToCart(item)
+  const shoppingCart = utils.getShoppingCart()
+  expect(shoppingCart).to.be.a("Array")
+  expect(shoppingCart).with.lengthOf(1)
+})
 
-it("Should return the number of items in the cart")
+it("Should add a new item to the shopping cart", function(){
+  const item = utils.createItem("apple", 0.99)
+  utils.addItemToCart(item)
+  const shoppingCart = utils.getShoppingCart()
+  const appleInCard = shoppingCart.map(function(e) { return e.name; }).indexOf('apple');
+  expect(appleInCard).to.equal(0)
+})
 
-it("Should remove items from cart")
+it("Should remove items from cart", function(){
+  const item = utils.createItem("apple", 0.99)
+  utils.addItemToCart(item)
+  utils.removeItemFromCart(item)
+  const shoppingCart = utils.getShoppingCart()
+  const appleInCard = shoppingCart.map(function(e) { return e.name; }).indexOf('apple');
+  expect(appleInCard).to.equal(-1) 
+})
 
 // ========================================================
 // Stretch Challenges
 // ========================================================
 
-it("Should update the count of items in the cart")
+it("Should update the count of items in the cart",function(){
+  const item = utils.createItem("apple", 0.99)
+  utils.addItemToCart(item)
+  const numOfItemsBeforeRemove = utils.getNumItemsInCart()
+  utils.removeItemFromCart(item)
+  const numOfItemsAfterRemove = utils.getNumItemsInCart()
+  expect(numOfItemsAfterRemove).to.equal(numOfItemsBeforeRemove - 1)
+  const numOfItemsBeforeAdd = utils.getNumItemsInCart()
+  utils.addItemToCart(item)
+  const numOfItemsAfterAdd = utils.getNumItemsInCart()
+  expect(numOfItemsAfterAdd).to.equal(numOfItemsBeforeAdd +1)
+})
 
-it("Should validate that an empty cart has 0 items")
+it("Should validate that an empty cart has 0 items",function(){
+  const item = utils.createItem("apple", 0.99)
+  utils.addItemToCart(item)
+  utils.clearCart()
+  const numOfItem = utils.getNumItemsInCart()
+  expect(numOfItem).to.be.a("number")
+  expect(numOfItem).to.equal(0)
+})
 
-it("Should return the total cost of all items in the cart")
+it("Should return the total cost of all items in the cart",function(){
+  const item = utils.createItem("apple", 0.99)
+  const item2 = utils.createItem("banana", 1.29)
+  utils.addItemToCart(item)
+  utils.addItemToCart(item2)
+  utils.addItemToCart(item)
+  const totalCost = utils.getTotalCost()
+  expect(totalCost).to.be.a("number")
+  expect(totalCost).to.equal(3.27)
+})
--- utils.js
+++ utils.js
@@ -40,22 +40,54 @@ const createItem = (name, price) => {
 
 const getShoppingCart = () => {
   // should return the current state of shopping cart
-}
-
-const addItemToCart = (item) => {
-  // should add item to shopping cart
+  return shoppingCart
 }
 
 const getNumItemsInCart = () => {
   // should return the total quantity of items in cart
+  sumOfItems = 0
+  shoppingCart.forEach((item)=>{
+    sumOfItems += item.quantity
+  })
+  return sumOfItems
+}
+
+const addItemToCart = (item) => {
+  // should add item to shopping cart
+  const pos = shoppingCart.map(function(e) { return e.name; }).indexOf(item.name);
+  if(pos === -1){
+    shoppingCart.push(item)
+  }else{
+    shoppingCart[pos].quantity += 1
+  }
 }
 
 const removeItemFromCart = (item) => {
   // should remove item from shopping cart
+  const pos = shoppingCart.map(function(e) { return e.name; }).indexOf(item.name);
+  if(pos === -1){
+    shoppingCart.push(item)
+  }else{
+    if(shoppingCart[pos].quantity === 1){
+      shoppingCart.splice(pos,1)
+    }else{
+      shoppingCart[pos].quantity -= 1
+    } 
+  }
 }
+
+const getTotalCost = ()=>{
+  // Should return the total cost of all items in the cart
+  totalCost = 0
+  shoppingCart.forEach((item)=>{
+    totalCost += item.price * item.quantity
+  })
+  return totalCost
+}
+
 
 module.exports = {
   sayHello, area, perimeter, circleArea,
   clearCart, createItem, getShoppingCart, addItemToCart,
-  getNumItemsInCart, removeItemFromCart
+  getNumItemsInCart, removeItemFromCart, getTotalCost
 }
